fece65af91531f5236ddc1af2e0c9408cf4e0980
j-d-b <jacob@jdbrady.info>
2018-09-10 13:43:47 -0400

    adding title to admin page

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 import UserList from '../components/UserList';
 
-const Admin = () => <UserList />
+const Admin = () => (
+  <div>
+    <h1>Users</h1>
+    <UserList />
+  </div>
+);
 
 export default Admin;
